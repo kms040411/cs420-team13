74cbe892f226a34c8cd2d6a80cae2784f8aec737
MinsuKim <kms040411@naver.com>
2019-12-07 19:44:18 +0900

    optimize skeleton

--- optimize.py
+++ optimize.py
@@ -1,9 +1,17 @@
 '''
-    optimize(tree):
-    Optimize the given tree and return optimized tree.
+    optimize(text):
+    Optimize the given text and return optimized text.
 '''
-def optimize(tree):
+def optimize(text):
     '''
         @TODO: Implement optimize()
     '''
-    return tree
+    result = dead_code_elimination(text)
+    result = loop_unrolling(text)
+    return result
+
+def dead_code_elimination(text):
+    pass
+
+def loop_unrolling(text):
+    pass
